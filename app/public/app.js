cd0e6c064233075d88b6a1ce325aa667433265dc
Joe Fitzpatrick <joe@Joes-MacBook-Pro.local>
2018-02-22 23:37:26 -0600

    creates object with lowest absolute values

---
+++ app/public/app.js
@@ -0,0 +1,21 @@
+var submission = {};
+
+$("#submit").on("click",function(){
+    
+    submission = {
+        sleepy: $("#sleepy input[type=radio]:checked").val(),
+        adventurous: $("#adventurous input[type=radio]:checked").val(),
+        thoughtful: $("#thoughtful input[type=radio]:checked").val(),
+        goaloriented: $("#goaloriented input[type=radio]:checked").val(),
+        ambitious: $("#ambitious input[type=radio]:checked").val(),
+        curious: $("#curious input[type=radio]:checked").val(),
+        selfpossessed: $("#selfpossessed input[type=radio]:checked").val(),
+        social: $("#social input[type=radio]:checked").val(),
+        frivolous: $("#frivolous input[type=radio]:checked").val(),
+        generous: $("#generous input[type=radio]:checked").val()
+    };
+    console.log(submission);
+
+    $.post("/api/submission", submission);
+});
+
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,5 @@
 var results = require("../data/friends.js");
+var submission = require("../data/submission.js");
 
 module.exports = function(app) {
     // API GET Requests
@@ -7,8 +8,12 @@ module.exports = function(app) {
     // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
     // ---------------------------------------------------------------------------
   
-    app.get("/api/friends", function(req, res) {
+    app.get("/api/results", function(req, res) {
       res.json(results);
+    });
+
+    app.get("/api/submission", function(req, res) {
+      res.json(submission);
     });
   
     // API POST Requests
@@ -19,21 +24,72 @@ module.exports = function(app) {
     // Then the server saves the data to the tableData array)
     // ---------------------------------------------------------------------------
   
-    // app.post("/api/friends", function(req, res) {
+    app.post("/api/submission", function(req, res) {
       // req.body is available since we're using the body-parser middleware
-      // if (tableData.length < 5) {
-      //   tableData.push(req.body);
-      //   res.json(true);
-      // }
-      // else {
-      //   waitListData.push(req.body);
-      //   res.json(false);
-      // }
+      submission.sleepy = parseInt(req.body.sleepy);
+      submission.adventurous = parseInt(req.body.adventurous);
+      submission.thoughtful = parseInt(req.body.thoughtful);
+      submission.goaloriented = parseInt(req.body.goaloriented);
+      submission.ambitious = parseInt(req.body.ambitious);
+      submission.curious = parseInt(req.body.curious);
+      submission.selfpossessed = parseInt(req.body.selfpossessed);
+      submission.social = parseInt(req.body.social);
+      submission.frivolous = parseInt(req.body.frivolous);
+      submission.generous = parseInt(req.body.generous);
+  
+      compareObj = {};
+      for(var key in results){
+        compareObj[results[key].name] = {};
+        compareObj[results[key].name].compareValues = {};
 
-      
+        compareObj[results[key].name].need = results[key].need;
+        compareObj[results[key].name].img = results[key].img;
 
-      
-    // });
+        // console.log("compareObj: ", compareObj);
+        compareObj[results[key].name].compareValues.sleepy = Math.abs(results[key].sleepy - submission.sleepy);
+        compareObj[results[key].name].compareValues.adventurous = Math.abs(results[key].adventurous - submission.adventurous);
+        compareObj[results[key].name].compareValues.thoughtful = Math.abs(results[key].thoughtful - submission.thoughtful);
+        compareObj[results[key].name].compareValues.goaloriented = Math.abs(results[key].goaloriented - submission.goaloriented);
+        compareObj[results[key].name].compareValues.ambitious = Math.abs(results[key].ambitious - submission.ambitious);
+        compareObj[results[key].name].compareValues.curious = Math.abs(results[key].curious - submission.curious);
+        compareObj[results[key].name].compareValues.selfpossessed = Math.abs(results[key].selfpossessed - submission.selfpossessed);
+        compareObj[results[key].name].compareValues.social = Math.abs(results[key].social - submission.social);
+        compareObj[results[key].name].compareValues.frivolous = Math.abs(results[key].frivolous - submission.frivolous);
+        compareObj[results[key].name].compareValues.generous = Math.abs(results[key].generous - submission.generous);
+
+
+        compareObj[results[key].name].compareTotal = compareObj[results[key].name].compareValues.sleepy +
+        compareObj[results[key].name].compareValues.adventurous +
+        compareObj[results[key].name].compareValues.thoughtful +
+        compareObj[results[key].name].compareValues.goaloriented +
+        compareObj[results[key].name].compareValues.ambitious +
+        compareObj[results[key].name].compareValues.curious +
+        compareObj[results[key].name].compareValues.selfpossessed +
+        compareObj[results[key].name].compareValues.social +
+        compareObj[results[key].name].compareValues.frivolous +
+        compareObj[results[key].name].compareValues.generous;
+
+      }
+      console.log(compareObj);
+
+      var lowestCompare = {
+        compareTotal: 100,
+        need: "",
+        img: ""
+      };
+
+      for (var key in compareObj) {
+        if (compareObj[key].compareTotal < lowestCompare.compareTotal){
+          lowestCompare.compareTotal = compareObj[key].compareTotal;
+          lowestCompare.need = compareObj[key].need;
+          lowestCompare.img = compareObj[key].img;
+        }
+      }
+
+      console.log("lowestCompare: ", lowestCompare);
+      res.json(lowestCompare);
+      res.end();
+    });
   
     // ---------------------------------------------------------------------------
     // I added this below code so you could clear out the table while working with the functionality.
